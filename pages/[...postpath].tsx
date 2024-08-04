2d1e1ebf16fa645a7b4bf75e3efaf63d9c5c3077
softweario <fa03119999669@gmail.com>
2024-08-04 11:44:15 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://wyseducation.xyz/graphql"
+	const endpoint = "https://softweario.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://wyseducation.xyz/` + encodeURI(path as string)
+					`https://softweario.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
